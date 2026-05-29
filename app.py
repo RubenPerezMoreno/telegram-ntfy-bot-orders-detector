9cc2f58a6adbd269ad0e5c0457b89fa86552c6e9
RubenPerezMoreno <ruben.perez@alumnojoyfe.iepgroup.es>
2026-05-29 22:24:03 +0100

    Agregar servidor web con Flask para cumplir con los requisitos de Render.com - Web Service

--- app.py
+++ app.py
@@ -209,3 +209,21 @@ print("===================================\n")
 
 client.start()
 client.run_until_disconnected()
+
+
+# =========================================
+# WEB SERVER - Port Binding for Render.com
+# =========================================
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Bot is running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+# =========================
